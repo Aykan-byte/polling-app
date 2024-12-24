d0424719a9411cbc61ce7100955d7c394eb61a9a
Mahmut Elmusa <mahmooda2253@gmail.com>
2024-12-24 12:47:40 +0300

    Exception

---
+++ src/main/java/com/proje/exception/ErrorMessage.java
@@ -0,0 +1,40 @@
+package com.proje.exception;
+
+public class ErrorMessage {private MessageType messageType;
+
+    private String ofStatic;
+
+
+
+    public String prepareErrorMessage(){
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(messageType.getMessage());
+        if (ofStatic!=null){
+            builder.append(" : " + ofStatic);
+        }
+        return builder.toString();
+
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getOfStatic() {
+        return ofStatic;
+    }
+
+    public void setOfStatic(String ofStatic) {
+        this.ofStatic = ofStatic;
+    }
+
+    public ErrorMessage(MessageType messageType, String ofStatic) {
+        this.messageType = messageType;
+        this.ofStatic = ofStatic;
+    }
+}
---
+++ src/main/java/com/proje/model/RootEntity.java
@@ -0,0 +1,54 @@
+package com.proje.model;
+
+public class RootEntity<T> {
+    private boolean result;
+
+    private String errorMessage;
+
+    private T data;
+
+
+    public static <T>RootEntity<T> ok(T data){
+
+        RootEntity<T>rootEntity=new RootEntity<>();
+        rootEntity.setData(data);
+        rootEntity.setResult(true);
+        rootEntity.setErrorMessage(null);
+
+        return rootEntity;
+    }
+
+    public static <T>RootEntity<T> error(String errorMessage){
+
+        RootEntity<T>rootEntity=new RootEntity<>();
+        rootEntity.setData(null);
+        rootEntity.setErrorMessage(errorMessage);
+        rootEntity.setResult(false);
+
+        return rootEntity;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
